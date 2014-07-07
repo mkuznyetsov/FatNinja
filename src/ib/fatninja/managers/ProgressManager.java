0c4af070897e9c452802233536626f29c0f1c6a7
Bogdan <Bogdan@WINCTRL-DMSKV7I>
2014-07-07 21:53:28 +0300

    x

---
+++ src/ib/fatninja/managers/ProgressManager.java
@@ -0,0 +1,33 @@
+package ib.fatninja.managers;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class ProgressManager {
+
+	private static ProgressManager instance;
+	
+	public static ProgressManager Instance(){
+		if(instance == null)
+			instance = new ProgressManager();
+		return instance;
+	}
+	
+	public void Save(){
+		try {
+			FileOutputStream fos = new FileOutputStream("progress.dat");
+			fos.write(buffer)
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	public void Load(){
+		
+		
+	}
+
+}
